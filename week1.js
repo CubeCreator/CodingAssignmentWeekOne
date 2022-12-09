d208420e14b643a09e40a0a9bde892d7cb79916c
Simon Lucas <simonlucas@hotmail.ca>
2022-12-09 14:14:37 -0500

    first time

---
+++ week1.js
@@ -0,0 +1,7 @@
+console.log("This is a console.")
+var FirstName = "Simon"
+var LastName = "Lucas"
+var Age = 21
+var IsGamer = true
+console.log("My Name is ", FirstName, " ", LastName, " Aged ", Age, " I made this.")
+console.log("Fact: I am a gamer = ", IsGamer)

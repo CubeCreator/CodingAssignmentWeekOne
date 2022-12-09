2b395a49e367755bc02ba7a38d24e611f509f06a
Simon Lucas <simonlucas@hotmail.ca>
2022-12-09 14:35:14 -0500

    second try

--- week1.js
+++ week1.js
@@ -1,7 +1,8 @@
 console.log("This is a console.")
 var FirstName = "Simon"
 var LastName = "Lucas"
+var MiddleName = "Peter John"
 var Age = 21
 var IsGamer = true
-console.log("My Name is ", FirstName, " ", LastName, " Aged ", Age, " I made this.")
+console.log("My Name is ", FirstName, " ", MiddleName, " ", LastName, " Aged ", Age, " I made this.")
 console.log("Fact: I am a gamer = ", IsGamer)
